9a40c5d0f01a9203787208e4cd2e7ba1db57d073
rajgopalhota <rajgopalhota@gmail.com>
2025-11-06 22:10:09 +0530

    Setup done

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,7 +24,13 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="min-h-screen bg-white">
+          <Header />
+          <main className='pt-10'>
+            {children}
+          </main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
 import HeroParallax from '@/components/home/HeroParallax';
 import Features from '@/components/home/Features';
 import ProcessParallax from '@/components/home/ProcessParallax';
@@ -9,15 +7,11 @@ import CTA from '@/components/home/CTA';
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
-      <Header />
-      <main className='pt-10'>
-        <HeroParallax />
-        <Features />
-        <ProcessParallax />
-        <ParallaxStats />
-        <CTA />
-      </main>
-      <Footer />
+      <HeroParallax />
+      <Features />
+      <ProcessParallax />
+      <ParallaxStats />
+      <CTA />
     </div>
   );
 }
